41918e400a4212f810b14d1d61f0ee5b0d18d22a
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-06-09 21:37:34 +0300

    Добавьте файлы проекта.

---
+++ Task17_3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<cstring>
+bool search(char *a, char *b, int sizeA, int sizeB )
+{
+    std::string av = a;
+    for (int i = 0; i < sizeA; ++i) 
+    {
+       if (av.substr(i, sizeB) == b) 
+            return true;
+    }
+    return false;
+}
+int main()
+{
+	char a[] = "Hello world";
+
+	char b[] = "wo";
+    int sizeA = sizeof(a)-1;
+    int sizeB = sizeof(b)-1;
+    std::cout << search(a, b, sizeA,sizeB);
+}
+
